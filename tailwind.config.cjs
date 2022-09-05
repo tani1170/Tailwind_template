13ff1950c5e5df60e5bf1a706301b4e5bcb36ca5
Your Name <you@example.com>
2022-09-05 15:14:16 +0200

    gloria knapper + animation, farve og cursor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      cursor: {
+        curly:
+          "url(img/icons8-curly-arrow-70.png), pointer",
+        sling:
+          "url(img/icons8-squiggly-arrow-70.png) 55 0, pointer",
+        mypointer:
+          "url(img/icons8-cursor-65.png), pointer",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+  ],
 };
